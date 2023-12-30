2e6e7b4fc04f60a0140104791a0a95cb1aa9824c
roqaiyakhanum <roqaiya.khanum@gmail.com>
2023-12-30 21:43:45 +0530

    ResponseStatusException first commit

--- src/main/java/com/stacksimplify/restservices/controller/EmployeeController.java
+++ src/main/java/com/stacksimplify/restservices/controller/EmployeeController.java
@@ -4,6 +4,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,8 +14,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.stacksimplify.restservices.entities.Employee;
+import com.stacksimplify.restservices.exception.UserExistsException;
+import com.stacksimplify.restservices.exception.UserNotFoundException;
 import com.stacksimplify.restservices.services.EmployeeService;
 
 @RestController
@@ -30,22 +37,40 @@ public class EmployeeController {
 	}
 	
 	@PostMapping("/createUser")
-	public Employee createEmployee(@RequestBody Employee emp) {
+	public ResponseEntity<Void> createEmployee(@RequestBody Employee emp,UriComponentsBuilder builder) {
 		
-		return employeeService.createUser(emp);
+		try {
+			 employeeService.createUser(emp);
+			 
+			 HttpHeaders headers= new HttpHeaders();
+			 headers.setLocation(builder.path("/user/{id}").buildAndExpand(emp.getId()).toUri());
+			 return new ResponseEntity<Void>(headers,HttpStatus.CREATED);
+			 
+		} catch (UserExistsException e) {
+			
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST,e.getMessage());
+		}
 	}
 	
 	
 	@GetMapping("/user/{id}")
 	public Optional<Employee> getEmployeeById(@PathVariable("id") Long id) {
 		
-		return employeeService.getUserById(id);
+		try {
+			return employeeService.getUserById(id);
+		} catch (UserNotFoundException e) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND,e.getMessage());
+		}
 		
 	}
 	@PutMapping("/user/{id}")
 	public Employee updateEmployee(@RequestBody Employee emp,@PathVariable("id") Long id) {
 		
-		return employeeService.updateEmployeeById(emp, id);
+		try {
+			return employeeService.updateEmployeeById(emp, id);
+		} catch (UserNotFoundException e) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST,e.getMessage());
+		}
 		
 	}
 	
